375385e73a77789120ecce28a4bc37f4931092ef
Teddy351 <143319165+Teddy351@users.noreply.github.com>
2023-08-30 17:48:45 +0900

    Add files via upload

---
+++ CurvedRanger/SectionOne/Step.cpp
@@ -0,0 +1,74 @@
+#include "Step.h"
+#include "Arduino.h"
+
+Step_M::Step_M(byte dir,byte step){
+    pinMode(dir,OUTPUT);
+    pinMode(step,OUTPUT);
+    //pinMode(en,OUTPUT);
+
+    this->dir = dir;
+    this->step = step;
+    //this->en = en;
+
+    this->pre_step = 0;
+    //digitalWrite(this->en,HIGH);
+    digitalWrite(this->dir,LOW);
+    digitalWrite(this->step,LOW);
+}
+
+void Step_M::setStep(long steps){
+    if(steps >= 0){
+        this->STEP = steps;
+        digitalWrite(this->dir,HIGH);
+    }
+    else{
+        this->STEP = -steps;
+        digitalWrite(this->dir,LOW);
+    }
+    //digitalWrite(this->en,LOW);
+}
+
+void Step_M::setDuration(long duration){
+    this->duration = duration;
+}
+
+bool Step_M::Whether(){
+    if(this->STEP){
+        return 1;
+    }
+    else{
+        //digitalWrite(this->en,HIGH);
+        return 0;
+    }
+}
+
+void Step_M::Move(){
+    if(this->STEP){
+        this->cur = micros();
+        if(this->cur - this->pre >= this->duration){
+            this->pre = this->cur;
+            if(this->state){
+                this->state = 0;
+            }
+            else{
+                this->state = 1;
+                this->STEP--;
+            }
+            digitalWrite(this->step,this->state);
+        }
+    }
+}
+
+void Step_M::Just_Move(){
+    digitalWrite(this->step,1);
+    delayMicroseconds(this->duration);
+    digitalWrite(this->step,0);
+    delayMicroseconds(this->duration);
+}
+
+long Step_M::preStep(long steps){
+    long last = steps - this->pre_step;
+    this->pre_step = steps;
+    Serial.println(last);
+    return last;
+}
---
+++ CurvedRanger/SectionOne/Step.h
@@ -0,0 +1,25 @@
+#ifndef __STEP_H__
+#define __STEP_H__
+#include "Arduino.h"
+class Step_M{
+    private:
+        byte dir,step;
+        long STEP;
+        bool DIR;
+        //bool EN;
+        long pre_step = 0;
+        unsigned long cur = 0;
+        unsigned long pre = 0;
+        bool state = 0;
+        long duration = 100;
+    
+    public:
+        Step_M(byte dir,byte step);
+        void setStep(long steps);
+        void setDuration(long duration);
+        bool Whether();
+        void Move();
+        void Just_Move();
+        long preStep(long steps);
+};
+#endif
